8edb6ca43deda2755726d8da1b072f95aeb0c9d4
sneo003 <doudotdou@hotmail.com>
2021-03-25 14:02:54 +0800

    Solution for part3-5 and part3-6
    part3.5 Expand the backend so that new phonebook entries can be added by making HTTP POST requests to the address http://localhost:3001/api/persons.
    Generate a new id for the phonebook entry with the Math.random function. 
    
    part3.6 Implement error handling for creating new entries. The request is not allowed to succeed, if:
    The name or number is missing
    The name already exists in the phonebook

---
+++ index.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+
+let persons = [
+    {
+        id: 1,
+        name: "Arto Hellas",
+        number: "040-123456"
+    },
+    {
+        id: 2,
+        name: "Ada Lovelace",
+        number: "39-44-5323523"
+    },
+    {
+        id: 3,
+        name: "Dan Abramov",
+        number: "12-43-234345"
+    },
+    {
+        id: 4,
+        name: "Mary Poppendick",
+        number: "39-23-6423122"
+    }
+]
+
+app.get('/info', (request, response) => {
+    const personsLen = persons.length
+    const date = new Date()
+    var summary = `Phonebook has info for ${personsLen} persons\n\n`
+    summary = summary.concat(date)
+    response.send(summary)
+})
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if(person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+})
+
+app.post('/api/persons/', (request, response) => {
+    const ranId = Math.floor(Math.random() * Math.floor(9999))
+    const body = request.body
+
+    if (!body.name) {
+        return response.status(400).json({
+            error: 'name missing'
+        })
+    } else if (!body.number) {
+        return response.status(400).json({
+            error: 'number missing'
+        })
+    } else if (persons.find(duplicate => duplicate.name === body.name)) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+
+    const person = {
+        id: ranId,
+        name: body.name,
+        number: body.number,
+    }
+    persons = persons.concat(person)
+
+    response.json(person)
+   
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
