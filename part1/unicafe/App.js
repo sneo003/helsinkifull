694a03c363e096ccb463173d45868a0c72f6065c
sneo003 <doudotdou@hotmail.com>
2021-03-28 10:59:18 +0800

    answer to 1.7: unicafe step 2

--- part1/unicafe/App.js
+++ part1/unicafe/App.js
@@ -3,13 +3,18 @@ import React, { useState } from 'react'
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
 )
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
   
-  const handleFeedback = (setFeedback, feedback) => { setFeedback(feedback + 1) }
+  const handleFeedback = (setFeedback, feedback) => { 
+    setFeedback(feedback + 1)
+    setTotal(total + 1)
+  }
 
   return (
     <div>
@@ -20,7 +25,11 @@ const App = () => {
       <h1>statistics</h1>
       <p>good {good}<br />
       neutral {neutral}<br />
-      bad {bad}</p>
+      bad {bad}<br />
+      all {total}<br />
+      average {(good-bad)/total || 0}<br />
+      positive {(good/total)*100 || 0} %
+      </p>
     </div>
   )
 }
