b2719b86493fe8019dd5a8fab0d58c16f7dcbc7f
sneo003 <doudotdou@hotmail.com>
2021-03-29 20:09:42 +0800

    answer for exercise 2.1: Course information step6 add package.json and component files

---
+++ part2/src/components/Content.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Part from './Part'
+
+const Content = ({ part }) => {
+    return (
+      <div>
+        {part.map(part =>
+        <Part key={part.id} part={part} />
+        )}
+      </div>
+    )
+}
+
+export default Content
