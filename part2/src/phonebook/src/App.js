8e68d406c49eda44448cb28510e6d9785d98380d
sneo003 <doudotdou@hotmail.com>
2021-03-30 19:32:05 +0800

    created App.js, index.js for exercise 2.6

---
+++ part2/src/phonebook/src/App.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+
+const Person = ({ name }) => {
+  return (
+    <li style={{ listStyleType: "none" }}>{name.name}</li>
+  )
+}
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+  
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event);
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input 
+          value={newName}
+          onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2> 
+      <ul>
+        {persons.map(person =>
+        <Person key={person.name} name={person}/>
+        )}
+      </ul> 
+    </div>
+  )
+}
+      
+
+export default App
