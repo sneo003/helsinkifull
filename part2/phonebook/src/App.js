200a82a622ba5ff48ca4700c9245d808a64022c7
sneo003 <doudotdou@hotmail.com>
2021-03-30 20:22:34 +0800

    solution for 2.7: The Phonebook
    added alert if name already exits in phonebook.

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 
 const Person = ({ name }) => {
   return (
-    <li style={{ listStyleType: "none" }}>{name.name}</li>
+    <li style={{ listStyleType: 'none' }}>{name.name}</li>
   )
 }
 
@@ -17,12 +17,16 @@ const App = () => {
     const personObject = {
       name: newName
     }
-    setPersons(persons.concat(personObject))
+    if (persons.some((person) => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+    } else {
+      setPersons(persons.concat(personObject))
+    }
     setNewName('')
   }
 
   const handleNameChange = (event) => {
-    console.log(event);
+    // console.log(event.target.value);
     setNewName(event.target.value)
   }
 
@@ -41,7 +45,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2> 
-      <ul>
+      <ul style={{ paddingLeft: 0 }}>
         {persons.map(person =>
         <Person key={person.name} name={person}/>
         )}
